04f62dfcfab6e5310e7747ac11a20abba821b3c9
SamKaiYang <tt00621212@gmail.com>
2020-03-20 22:36:21 +0800

    fix calibration

--- src/Get_ID_tvec_and_rvec_depth.py
+++ src/Get_ID_tvec_and_rvec_depth.py
@@ -46,61 +46,6 @@ def ar_server():
     print ("Ready to connect")
     rospy.spin() ## spin one
 
-
-def calibrate():
-
-    cap = cv2.VideoCapture(2)# for realsense channel 2
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    # checkerboard of size (9 x 7) is used
-    objp = np.zeros((7*9,3), np.float32)
-    objp[:,:2] = np.mgrid[0:9,0:7].T.reshape(-1,2)
-
-    # arrays to store object points and image points from all the images.
-    objpoints = [] # 3d point in real world space
-    imgpoints = [] # 2d points in image plane.
-
-    while(True):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        # resizing for faster detection
-        frame = cv2.resize(frame, (640, 480))
-        # using a greyscale picture, also for faster detection
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Find the chessboard corners
-        ret, corners = cv2.findChessboardCorners(gray, (9,7), None)
-
-        # If found, add object points, image points
-        if ret == True:
-            objpoints.append(objp)
-            imgpoints.append(corners)
-
-            # Draw and display the corners
-            cv2.drawChessboardCorners(frame, (9,7), corners, ret)
-            #write_name = 'corners_found'+str(idx)+'.jpg'
-
-        # Display the resulting frame
-        cv2.imshow('Calibration',frame)
-        if cv2.waitKey(100) & 0xFF == ord('q'):
-            break
-        
-    cap.release()
-    cv2.destroyAllWindows()
-    cv2.waitKey(10)
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-    
-    #create a file to store data
-    from lxml import etree
-    from lxml.builder import E
-    
-    global fname
-    with open(fname, "w") as f:
-        f.write("{'ret':"+str(ret)+", 'mtx':"+str(list(mtx))+', "dist":'+str(list(dist))+'}')
-        f.close()
-
-
 def myhook():
     print ("shutdown time!")
 
@@ -159,8 +104,8 @@ if __name__ == '__main__':
     argv = rospy.myargv()
     rospy.init_node('ar_ros_server', anonymous=True)
 
-    t = threading.Thread(target=realsense_depth)
-    t.start() # 開啟多執行緒
+    # t = threading.Thread(target=realsense_depth)
+    # t.start() # 開啟多執行緒
 
 
 #test wheater already calibrated or not
@@ -173,7 +118,7 @@ if __name__ == '__main__':
         f.read()
         f.close()
     except:
-        calibrate()
+        print("You have not imported the calibration file")
   
 
     cap = cv2.VideoCapture(2)
@@ -216,9 +161,8 @@ if __name__ == '__main__':
             rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, 0.05, mtx, dist)
 
         #print(ids)
-        #print(corners)
+            #print(corners)
         #print(rvec)
-        
             for i in range(0, ids.size):
                 aruco.drawAxis(frame, mtx, dist, rvec[i], tvec[i], 0.1)
 
@@ -230,11 +174,15 @@ if __name__ == '__main__':
 
                 #get tvec, rvec of each id
                 # print(ids[i])
+                #print(rvec[i])
                 print(tvec[i][0])
-                print(rvec[i][0])
+                #print(rvec[i][0])
+
+                print(position)
             # print(ids)
             # print(tvec)
             # print(rvec)
+            #print(dist)
 
             aruco.drawDetectedMarkers(frame, corners)
 
@@ -245,7 +193,7 @@ if __name__ == '__main__':
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    t.join()
+    # t.join()
     cap.release()
     cv2.destroyAllWindows()
     rospy.on_shutdown(myhook)
